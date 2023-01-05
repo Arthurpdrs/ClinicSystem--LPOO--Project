d67026ec4f5060293b7a03270f9b15ca039681e3
Paulo André <pauloandrejanga@gmail.com>
2023-01-05 17:52:38 -0300

    Create JanelaVisualizarPacientes.java

---
+++ src/ui/JanelaVisualizarPacientes.java
@@ -0,0 +1,174 @@
+package ui;
+
+import java.awt.EventQueue;
+import java.awt.Insets;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Cursor;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import java.awt.Toolkit;
+
+public class JanelaVisualizarPacientes {
+
+	private JFrame frmClinicsystem;
+	private JLabel erroLbl;
+	private JLabel barraAzul;
+	private JLabel logoMenu;
+	private JLabel titulodapaginaLbl;
+	private JTextField codigoDoPacienteTextField;
+	private JLabel codigoDoPacienteLbl;
+	private JButton pesquisarBtn;
+	private JScrollPane scrollPane;
+	private JTable pacientesTable;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaVisualizarPacientes window = new JanelaVisualizarPacientes();
+					window.frmClinicsystem.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JanelaVisualizarPacientes() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmClinicsystem = new JFrame();
+		frmClinicsystem.setIconImage(Toolkit.getDefaultToolkit().getImage(JanelaVisualizarPacientes.class.getResource("/midia/icone.png")));
+		frmClinicsystem.setTitle("ClinicSystem");
+		frmClinicsystem.getContentPane().setBackground(new Color(255, 255, 255));
+		frmClinicsystem.getContentPane().setLayout(null);
+		
+		
+
+		scrollPane = new JScrollPane(pacientesTable = new JTable());
+		pacientesTable.setForeground(Color.GRAY);
+		pacientesTable.setFont(new Font("Arial", Font.PLAIN, 12));
+		pacientesTable.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Nome completo", "Endere\u00E7o", "CPF", "E-mail", "Celular", "Tipo sangu\u00EDneo", "Alergia", "Observa\u00E7\u00E3o", "Data de nascimento", "Respons\u00E1vel - Nome completo", "Respons\u00E1vel - Celular", "Respons\u00E1vel - CPF"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				true, true, false, true, true, true, true, true, true, true, true, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane.setBounds(31, 162, 1048, 356);
+		frmClinicsystem.getContentPane().add(scrollPane);
+		
+		titulodapaginaLbl = new JLabel("Visualizar pacientes");
+		titulodapaginaLbl.setHorizontalAlignment(SwingConstants.LEFT);
+		titulodapaginaLbl.setForeground(Color.WHITE);
+		titulodapaginaLbl.setFont(new Font("Arial", Font.PLAIN, 16));
+		titulodapaginaLbl.setBackground(Color.WHITE);
+		titulodapaginaLbl.setBounds(32, -1, 370, 55);
+		frmClinicsystem.getContentPane().add(titulodapaginaLbl);
+		
+		logoMenu = new JLabel("");
+		logoMenu.setIcon(new ImageIcon(JanelaVisualizarPacientes.class.getResource("/midia/logo_menu.png")));
+		logoMenu.setBounds(919, -1, 160, 55);
+		frmClinicsystem.getContentPane().add(logoMenu);
+		
+		barraAzul = new JLabel("");
+		barraAzul.setBackground(new Color(0, 102, 255));
+		barraAzul.setBounds(-1, -1, 1131, 55);
+		barraAzul.setOpaque(true);
+		frmClinicsystem.getContentPane().add(barraAzul);
+		
+		erroLbl = new JLabel("Editado com sucesso");
+		erroLbl.setVerticalAlignment(SwingConstants.BOTTOM);
+		erroLbl.setOpaque(true);
+		erroLbl.setHorizontalAlignment(SwingConstants.RIGHT);
+		erroLbl.setForeground(new Color(0, 102, 255));
+		erroLbl.setFont(new Font("Arial", Font.PLAIN, 14));
+		erroLbl.setBackground(Color.WHITE);
+		erroLbl.setBounds(415, 544, 664, 14);
+		frmClinicsystem.getContentPane().add(erroLbl);
+		
+		JButton editarBtn = new JButton("Editar");
+		editarBtn.setBorderPainted(false);
+		editarBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		editarBtn.setBackground(new Color(255, 255, 255));
+		editarBtn.setForeground(new Color(0, 102, 255));
+		editarBtn.setFont(new Font("Arial", Font.PLAIN, 16));
+		editarBtn.setBounds(997, 582, 89, 34);
+		editarBtn.setContentAreaFilled(false);
+		frmClinicsystem.getContentPane().add(editarBtn);
+		
+		codigoDoPacienteTextField = new JTextField();
+		codigoDoPacienteTextField.setToolTipText("");
+		codigoDoPacienteTextField.setMargin(new Insets(10, 10, 10, 10));
+		codigoDoPacienteTextField.setHorizontalAlignment(SwingConstants.LEFT);
+		codigoDoPacienteTextField.setForeground(Color.GRAY);
+		codigoDoPacienteTextField.setFont(new Font("Arial", Font.PLAIN, 12));
+		codigoDoPacienteTextField.setColumns(10);
+		codigoDoPacienteTextField.setBackground(Color.WHITE);
+		codigoDoPacienteTextField.setActionCommand("");
+		codigoDoPacienteTextField.setBounds(32, 100, 955, 50);
+		frmClinicsystem.getContentPane().add(codigoDoPacienteTextField);
+		
+		codigoDoPacienteLbl = new JLabel("Pesquisar CPF do paciente:");
+		codigoDoPacienteLbl.setHorizontalAlignment(SwingConstants.LEFT);
+		codigoDoPacienteLbl.setForeground(Color.GRAY);
+		codigoDoPacienteLbl.setFont(new Font("Arial", Font.PLAIN, 16));
+		codigoDoPacienteLbl.setBackground(Color.WHITE);
+		codigoDoPacienteLbl.setBounds(32, 65, 380, 24);
+		frmClinicsystem.getContentPane().add(codigoDoPacienteLbl);
+		
+		JButton excluirBtn = new JButton("Excluir");
+		excluirBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		excluirBtn.setForeground(Color.GRAY);
+		excluirBtn.setFont(new Font("Arial", Font.PLAIN, 16));
+		excluirBtn.setContentAreaFilled(false);
+		excluirBtn.setBorderPainted(false);
+		excluirBtn.setBackground(Color.WHITE);
+		excluirBtn.setBounds(898, 582, 89, 34);
+		frmClinicsystem.getContentPane().add(excluirBtn);
+		
+		pesquisarBtn = new JButton("");
+		pesquisarBtn.setIcon(new ImageIcon(JanelaVisualizarPacientes.class.getResource("/midia/lupa.png")));
+		pesquisarBtn.setForeground(new Color(0, 102, 255));
+		pesquisarBtn.setFont(new Font("Arial", Font.PLAIN, 16));
+		pesquisarBtn.setContentAreaFilled(false);
+		pesquisarBtn.setBorderPainted(false);
+		pesquisarBtn.setBackground(Color.WHITE);
+		pesquisarBtn.setBounds(997, 100, 97, 50);
+		frmClinicsystem.getContentPane().add(pesquisarBtn);
+		
+		
+		
+		frmClinicsystem.setBackground(new Color(255, 255, 255));
+		frmClinicsystem.setResizable(false);
+		frmClinicsystem.setBounds(100, 100, 1120, 680);
+		frmClinicsystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
