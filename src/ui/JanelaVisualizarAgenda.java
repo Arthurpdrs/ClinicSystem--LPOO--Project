941cfc68e7adc03f894538eefb1d367a6ab8a795
Paulo André <pauloandrejanga@gmail.com>
2023-01-06 09:50:01 -0300

    Create JanelaVisualizarAgenda.java

---
+++ src/ui/JanelaVisualizarAgenda.java
@@ -0,0 +1,117 @@
+package ui;
+
+import java.awt.EventQueue;
+import java.awt.Insets;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import java.awt.Toolkit;
+import com.toedter.calendar.JCalendar;
+
+public class JanelaVisualizarAgenda {
+
+	private JFrame frmClinicsystem;
+	private JLabel barraAzul;
+	private JLabel logoMenu;
+	private JLabel titulodapaginaLbl;
+	private JScrollPane scrollPane;
+	private JTable agendaTable;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaVisualizarAgenda window = new JanelaVisualizarAgenda();
+					window.frmClinicsystem.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JanelaVisualizarAgenda() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmClinicsystem = new JFrame();
+		frmClinicsystem.setIconImage(Toolkit.getDefaultToolkit().getImage(JanelaVisualizarAgenda.class.getResource("/midia/icone.png")));
+		frmClinicsystem.setTitle("ClinicSystem");
+		frmClinicsystem.getContentPane().setBackground(new Color(255, 255, 255));
+		frmClinicsystem.getContentPane().setLayout(null);
+		
+		
+
+		scrollPane = new JScrollPane(agendaTable = new JTable());
+		agendaTable.setForeground(Color.GRAY);
+		agendaTable.setFont(new Font("Arial", Font.PLAIN, 12));
+		agendaTable.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Hor\u00E1rio", "Paciente", "CPF", "Respons\u00E1vel", "Estado"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane.setBounds(31, 422, 1048, 184);
+		frmClinicsystem.getContentPane().add(scrollPane);
+		
+		titulodapaginaLbl = new JLabel("Visualizar agenda");
+		titulodapaginaLbl.setHorizontalAlignment(SwingConstants.LEFT);
+		titulodapaginaLbl.setForeground(Color.WHITE);
+		titulodapaginaLbl.setFont(new Font("Arial", Font.PLAIN, 16));
+		titulodapaginaLbl.setBackground(Color.WHITE);
+		titulodapaginaLbl.setBounds(31, -1, 371, 55);
+		frmClinicsystem.getContentPane().add(titulodapaginaLbl);
+		
+		logoMenu = new JLabel("");
+		logoMenu.setIcon(new ImageIcon(JanelaVisualizarAgenda.class.getResource("/midia/logo_menu.png")));
+		logoMenu.setBounds(919, -1, 160, 55);
+		frmClinicsystem.getContentPane().add(logoMenu);
+		
+		barraAzul = new JLabel("");
+		barraAzul.setBackground(new Color(0, 102, 255));
+		barraAzul.setBounds(-1, -1, 1131, 55);
+		barraAzul.setOpaque(true);
+		frmClinicsystem.getContentPane().add(barraAzul);
+		
+		JCalendar calendar = new JCalendar();
+		calendar.getMonthChooser().getSpinner().setFont(new Font("Arial", Font.PLAIN, 12));
+		calendar.getMonthChooser().setBackground(Color.WHITE);
+		calendar.setDecorationBackgroundColor(Color.WHITE);
+		calendar.setBounds(31, 65, 1048, 346);
+		frmClinicsystem.getContentPane().add(calendar);
+		
+		
+		
+		frmClinicsystem.setBackground(new Color(255, 255, 255));
+		frmClinicsystem.setResizable(false);
+		frmClinicsystem.setBounds(100, 100, 1120, 680);
+		frmClinicsystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
