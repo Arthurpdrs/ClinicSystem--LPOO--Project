2b6e1af863254f0ca4eb684b2544b6111246bc83
Augusto Melo <guga7msn@hotmail.com>
2023-01-11 23:32:27 -0300

    Adição de classes

---
+++ src/model/Endereco.java
@@ -0,0 +1,74 @@
+package model;
+
+
+
+
+
+public class Endereco {
+	
+	private String logradouro;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	
+	public Endereco(
+			String logradouro,
+			String numero,
+			String bairro,
+			String cidade,
+			String cep) {
+		
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+	
+	
+}
+
+
