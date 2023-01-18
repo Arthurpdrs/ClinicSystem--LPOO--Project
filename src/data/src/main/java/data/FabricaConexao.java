def7e7665012892c182827cf9573fd1d26628231
sykes-07 <81064629+sykes-07@users.noreply.github.com>
2023-01-18 00:45:49 -0300

    update FabricaConexao

--- src/data/src/main/java/data/FabricaConexao.java
+++ src/data/src/main/java/data/FabricaConexao.java
@@ -1,32 +1,42 @@
 package data;
 
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
+import java.sql.SQLException;
 
 public class FabricaConexao {
 	
-	private Conexao conexao =null;
+	private static final String usuario = "admin";
+	private static final String senha = "12345678";
+	private static final String url = "jdbc:mysql:aws://database-2.ckzqkucrjrxv.us-east-1.rds.amazonaws.com:3306/BancoLPOO"; 
+
 	
-	public Conexao gerarConexao() {
-		if(conexao == null) {
-			this.conexao = new Conexao();
-			System.out.println("conexao bem sucedida");
+	public String getConnectoin(){
+	
+		try {
+			Connection conexao = DriverManager.getConnection(url, usuario, senha);
+			PreparedStatement pstm = conexao.prepareStatement("INSERT INTO README(id) VALUES ('123444')");
+
+			pstm.execute();
+
+			return "banco conectado";
+		
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 		
-		return this.conexao;
 	}
 	
 	
+	
+	
+	
+	public static void main(String [] args ) {
+		FabricaConexao testeconexao = new FabricaConexao();
+		testeconexao.getConnectoin();
 		
 
-	
-	
-	
-	public PreparedStatement prepareStatement(String sql) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 		
 	
