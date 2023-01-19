ca25fcf111c6035bd58a634c8ebfb70914fa92a7
Diana Correia <81064629+sykes-07@users.noreply.github.com>
2023-01-18 21:35:56 -0300

    Delete src directory

--- src/model/Recepcionista.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Recepcionista extends Funcionario{
-	
-	public Recepcionista(String nome, String telefone, String email, String cpf, String login, String senha, String matricula) {
-		super(nome, telefone, email, cpf, login, senha);
-	}
-
-}
