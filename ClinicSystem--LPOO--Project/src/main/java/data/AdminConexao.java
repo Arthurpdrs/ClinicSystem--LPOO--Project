19f82c44192da6bdb1bc9ab188d8f4f4a7cbb1d0
sykes-07 <81064629+sykes-07@users.noreply.github.com>
2023-01-23 22:53:10 -0300

    update classAdminConexao

--- ClinicSystem--LPOO--Project/src/main/java/data/AdminConexao.java
+++ ClinicSystem--LPOO--Project/src/main/java/data/AdminConexao.java
@@ -1,36 +1,50 @@
 package data;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AdminConexao extends FabricaConexao {
 	
 	FabricaConexao admin = new FabricaConexao();
 	
-	public void verificarLogin(String senha, String login){
-		String senhaAdmin = senha;
-		String loginAdmin = login;
-		String select = "SELECT "+ senhaAdmin + ", "+ loginAdmin +"FROM README";
-		admin.getConexao();
-		System.out.print("Pegou ");
+	public boolean verificarLoginAdmin(String senha, String login) throws SQLException{
+		try {
+			PreparedStatement select = admin.getConexao().prepareStatement("SELECT Login, Senha FROM Administrador u WHERE u.Login = ? AND u.Senha = ?");
+			select.setString(1, login);
+			select.setNString(2, senha);
+			return select.execute();
+		} finally {
+			admin.fecharConexao();
+		}
 	
 			
 	}
 	
 	
-	public String inserirFuncionario(String senha, String login ) {
-		String senhaFuncionario = senha;
-		String loginFuncionario = login;
-		String insertFuncionario = "INSERT INTO "+ senhaFuncionario + "(id) "+ "VALUES('"+ loginFuncionario+"')";
-		admin.getConexao(insertFuncionario);
-		
-		return null;
+	public boolean addFunionario(String Login, String Senha, String Pessoa_CPF) throws SQLException {
+		try {
+			PreparedStatement insert = admin.getConexao().prepareStatement("INSERT INTO Funcionario(Login, Senha) VALUES(?, ?)");
+			insert.setString(1, Login);
+			insert.setString(2, Senha);
+			insert.setString(3, Pessoa_CPF);
+			return insert.execute();	
+		} finally {
+			admin.fecharConexao();
 		}
-	
-	
-	public String deletarFuncionario(String CPF) {
-		String cpfFuncionario = CPF;
-		String insert = "DELETE FROM Funcionario WHERE Pessoa_CPF = "+ cpfFuncionario;  
-		admin.getConexao(insert);
 		
-		return null;}
+	}
+	
+	
+	public boolean deletarFuncionario(String Pessoa_CPF) throws SQLException{
+		try {
+			PreparedStatement insert = admin.getConexao().prepareStatement("DELETE FROM Funcionario WHERE Pessoa_CPF = ?");
+			insert.setString(1, Pessoa_CPF);
+			return insert.execute();	
+		} finally {
+			admin.fecharConexao();
+		}
+		
+	}
 
 }
 
