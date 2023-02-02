fe34fd9d89d4bc82ccd9b50c3712da3fcbdae5f0
gabrielvaz081 <vazgabriel081@gmail.com>
2023-02-02 20:03:22 -0300

    Atualização ConsultaDAO e ProntuarioDAO

--- ClinicSystem--LPOO--Project/src/main/java/dao/ConsultaDAO.java
+++ ClinicSystem--LPOO--Project/src/main/java/dao/ConsultaDAO.java
@@ -12,88 +12,65 @@ import core.model.Medico;
 import core.model.Paciente;
 import core.model.Pessoa;
 
-
 public class ConsultaDAO {
 	
 	FabricaConexao consultaDAO = new FabricaConexao();
 	
-    public List<Consulta> listar() {
+    public List<Consulta> listar() throws SQLException {
     	String sql = "SELECT * FROM Consulta";
 		List<Consulta> retorno = new ArrayList();
-		try {
-			PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
-			ResultSet resultado = stmt.executeQuery();
-			while(resultado.next()) {
-				
-				Consulta consulta = new Consulta();
-				Paciente paciente = new Paciente();
-				Medico medico = new Medico();
-;				
-				
-				consulta.setPaciente(paciente);
-				paciente.setCpf(resultado.getString("Pessoa_CPF"));
-				consulta.setMedico(medico);
-				medico.setCpf(resultado.getString("Funcionario_Pessoa_CPF"));
-				consulta.setDataConsulta(resultado.getDate("Data_consulta"));
-				consulta.setValor(resultado.getDouble("Valor"));
-				consulta.setPago(resultado.getBoolean("Pago"));
-				consulta.setHorario(resultado.getTime("Hora_consulta"));
-				retorno.add(consulta);
-			}
-		} catch (SQLException e) {
-			System.out.println("Não foi possível listar os dados da consulta!");
+		PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
+		ResultSet resultado = stmt.executeQuery();
+		while(resultado.next()) {
+			Consulta consulta = new Consulta();
+			Paciente paciente = new Paciente();
+			Medico medico = new Medico();
+			consulta.setPaciente(paciente);
+			paciente.setCpf(resultado.getString("Paciente_CPF"));
+			consulta.setMedico(medico);
+			medico.setCpf(resultado.getString("Medico_CPF"));
+			consulta.setDataConsulta(resultado.getDate("Data_consulta"));
+			consulta.setValor(resultado.getDouble("Valor"));
+			consulta.setPago(resultado.getBoolean("Pago"));
+			consulta.setHorario(resultado.getTime("Hora_consulta"));
+			retorno.add(consulta);
 		}
+		consultaDAO.fecharConexao();
 		return retorno;
 	}
 	
-	public boolean inserir(Consulta consulta) {
-		String sql = "INSERT INTO Consulta(Paciente_Pessoa_CPF, Medico_Funcionario_Pessoa_CPF, Data_consulta, Valor, Pago, Hora_consulta) VALUES(?, ?, ?, ?, ?, ?)";
-		try {
-			PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
-			
-			stmt.setString(1, consulta.getPaciente().getCpf());
-			stmt.setString(2, consulta.getMedico().getCpf());
-			stmt.setDate(3, consulta.getDataConsulta());
-			stmt.setDouble(4, consulta.getValor());
-			stmt.setBoolean(5, consulta.getPago());
-			stmt.setTime(6, consulta.getHorario());
-			stmt.execute();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Não foi possível inserir os dados da consulta!");
-			return false;
-		}
+	public void inserir(Consulta consulta) throws SQLException {
+		String sql = "INSERT INTO Consulta(Paciente_CPF, Medico_CPF, Data_consulta, Valor, Pago, Hora_consulta) VALUES(?, ?, ?, ?, ?, ?)";
+		PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, consulta.getPaciente().getCpf());
+		stmt.setString(2, consulta.getMedico().getCpf());
+		stmt.setDate(3, consulta.getDataConsulta());
+		stmt.setDouble(4, consulta.getValor());
+		stmt.setBoolean(5, consulta.getPago());
+		stmt.setTime(6, consulta.getHorario());
+		stmt.execute();
+		consultaDAO.fecharConexao();
 	}
 	
-	public boolean alterar(Consulta consulta) {
-		String sql = "UPDATE Consulta SET Data_consulta=?, Valor=?, Pago=?, Hora_consulta=? WHERE (Paciente_Pessoa_CPF=? AND Medico_Funcionario_Pessoa_CPF=?)";
-		try {
-			PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
-			stmt.setString(1, consulta.getPaciente());
-			stmt.setString(2, consulta.getMedico());
-			stmt.setDate(3, consulta.getDataConsulta());
-			stmt.setDouble(4, consulta.getValor());
-			stmt.setBoolean(5, consulta.getPago());
-			stmt.setTime(6, consulta.getHorario());
-			stmt.execute();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Não foi possível alterar os dados da consulta!");
-			return false;
-		}
+	public void alterar(Consulta consulta) throws SQLException {
+		String sql = "UPDATE Consulta SET Data_consulta=?, Valor=?, Pago=?, Hora_consulta=? WHERE (Paciente_CPF=? AND Medico_CPF=?)";
+		PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, consulta.getPaciente().getCpf());
+		stmt.setString(2, consulta.getMedico().getCpf());
+		stmt.setDate(3, consulta.getDataConsulta());
+		stmt.setDouble(4, consulta.getValor());
+		stmt.setBoolean(5, consulta.getPago());
+		stmt.setTime(6, consulta.getHorario());
+		stmt.execute();
+		consultaDAO.fecharConexao();
 	}
 	
-	public boolean remover(Consulta consulta) {
-		String sql = "DELETE FROM Consulta WHERE (Paciente_Pessoa_CPF=? AND Medico_Funcionario_Pessoa_CPF=?)";
-		try {
-			PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
-			stmt.setString(1, consulta.getPaciente());
-			stmt.setString(2, consulta.getMedico());
-			stmt.execute();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Não foi possível deletar os dados da consulta!");
-			return false;
-		}
+	public void remover(Consulta consulta) throws SQLException {
+		String sql = "DELETE FROM Consulta WHERE (Paciente_CPF=? AND Medico_CPF=?)";
+		PreparedStatement stmt = consultaDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, consulta.getPaciente().getCpf());
+		stmt.setString(2, consulta.getMedico().getCpf());
+		stmt.execute();
+		consultaDAO.fecharConexao();
 	}
 }
--- ClinicSystem--LPOO--Project/src/main/java/dao/ProntuarioDAO.java
+++ ClinicSystem--LPOO--Project/src/main/java/dao/ProntuarioDAO.java
@@ -7,90 +7,69 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import core.model.Medico;
 import core.model.Paciente;
 import core.model.Prontuario;
-
 
 public class ProntuarioDAO {
 	
 	FabricaConexao prontuarioDAO = new FabricaConexao();
 	
-    public List<Prontuario> listar() {
+    public List<Prontuario> listar() throws SQLException {
     	String sql = "SELECT * FROM Prontuario";
 		List<Prontuario> retorno = new ArrayList();
-		try {
-			PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
-			ResultSet resultado = stmt.executeQuery();
-			while(resultado.next()) {
-				Prontuario prontuario = new Prontuario();
-				Paciente paciente = new Paciente();
-				Medico medico = new Medico();
-				
-				prontuario.setPaciente(paciente);
-				paciente.setCpf(resultado.getString("Pessoa_CPF"));
-				prontuario.setMedico(medico);
-				medico.setCpf(resultado.getString("Funcionario_Pessoa_CPF"));
-				prontuario.setQueixa(resultado.getString("Queixa"));
-				prontuario.setPrescricao(resultado.getString("Prescricao"));
-				prontuario.setObservacao(resultado.getString("Observacao"));
-				prontuario.setData(resultado.getDate("Data_prontuario"));
-				retorno.add(prontuario);
-			}
-		} catch (SQLException e) {
-			System.out.println("Não foi possível listar os dados do prontuário!");
+		PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
+		ResultSet resultado = stmt.executeQuery();
+		while(resultado.next()) {
+			Prontuario prontuario = new Prontuario();
+			Paciente paciente = new Paciente();
+			Medico medico = new Medico();
+			prontuario.setPaciente(paciente);
+			paciente.setCpf(resultado.getString("Paciente_CPF"));
+			prontuario.setMedico(medico);
+			medico.setCpf(resultado.getString("Medico_CPF"));
+			prontuario.setQueixa(resultado.getString("Queixa"));
+			prontuario.setPrescricao(resultado.getString("Prescricao"));
+			prontuario.setObservacao(resultado.getString("Observacao"));
+			prontuario.setData(resultado.getDate("Data_prontuario"));
+			retorno.add(prontuario);
 		}
+		prontuarioDAO.fecharConexao();
 		return retorno;
 	}
 	
-	public boolean inserir(Prontuario prontuario) {
-		String sql = "INSERT INTO Prontuario(Paciente_Pessoa_CPF, Medico_Funcionario_Pessoa_CPF, Queixa, Prescricao, Observacao, Data_prontuario) VALUES(?, ?, ?, ?, ?, ?)";
-		try {
-			PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
-			stmt.setString(1, prontuario.getPaciente());
-			stmt.setString(2, prontuario.getMedico());
-			stmt.setString(3, prontuario.getQueixa());
-			stmt.setString(4, prontuario.getPrescricao());
-			stmt.setString(5, prontuario.getObservacao());
-			stmt.setDate(6, prontuario.getData());
-			stmt.execute();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Não foi possível inserir os dados do prontuário!");
-			return false;
-		}
+	public void inserir(Prontuario prontuario) throws SQLException {
+		String sql = "INSERT INTO Prontuario(Paciente_CPF, Medico_CPF, Queixa, Prescricao, Observacao, Data_prontuario) VALUES(?, ?, ?, ?, ?, ?)";
+		PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, prontuario.getPaciente().getCpf());
+		stmt.setString(2, prontuario.getMedico().getCpf());
+		stmt.setString(3, prontuario.getQueixa());
+		stmt.setString(4, prontuario.getPrescricao());
+		stmt.setString(5, prontuario.getObservacao());
+		stmt.setDate(6, prontuario.getData());
+		stmt.execute();
+		prontuarioDAO.fecharConexao();
 	}
 	
-	public boolean alterar(Prontuario prontuario) {
-		String sql = "UPDATE Prontuario SET Queixa=?, Prescricao=?, Observacao=?, Data_prontuario=? WHERE (Paciente_Pessoa_CPF=? AND Medico_Funcionario_Pessoa_CPF=?)";
-		try {
-			PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
-			stmt.setString(3, prontuario.getQueixa());
-			stmt.setString(4, prontuario.getPrescricao());
-			stmt.setString(5, prontuario.getObservacao());
-			stmt.setDate(6, prontuario.getData());
-			stmt.setString(1, prontuario.getPaciente());
-			stmt.setString(2, prontuario.getMedico());
-			stmt.execute();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Não foi possível alterar os dados do prontuário!");
-			return false;
-		}
+	public void alterar(Prontuario prontuario) throws SQLException {
+		String sql = "UPDATE Prontuario SET Queixa=?, Prescricao=?, Observacao=?, Data_prontuario=? WHERE (Paciente_CPF=? AND Medico_CPF=?)";
+		PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
+		stmt.setString(3, prontuario.getQueixa());
+		stmt.setString(4, prontuario.getPrescricao());
+		stmt.setString(5, prontuario.getObservacao());
+		stmt.setDate(6, prontuario.getData());
+		stmt.setString(1, prontuario.getPaciente().getCpf());
+		stmt.setString(2, prontuario.getMedico().getCpf());
+		stmt.execute();
+		prontuarioDAO.fecharConexao();
 	}
 	
-	public boolean remover(Prontuario prontuario) {
-		String sql = "DELETE FROM Prontuario WHERE (Paciente_Pessoa_Cpf=? AND Medico_Funcionario_Pessoa_CPF=?)";
-		try {
-			PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
-			stmt.setString(1, prontuario.getPaciente());
-			stmt.setString(2, prontuario.getMedico());
-			stmt.execute();
-			return true;
-		} catch (SQLException e) {
-			System.out.println("Não foi possível deletar os dados do prontuário!");
-			return false;
-		}
+	public void remover(Prontuario prontuario) throws SQLException {
+		String sql = "DELETE FROM Prontuario WHERE (Paciente_Cpf=? AND Medico_CPF=?)";
+		PreparedStatement stmt = prontuarioDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, prontuario.getPaciente().getCpf());
+		stmt.setString(2, prontuario.getMedico().getCpf());
+		stmt.execute();
+		prontuarioDAO.fecharConexao();
 	}
 }
