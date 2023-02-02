c9c8e57134843cebf180fb0039c99b209fd2e31b
gabrielvaz081 <vazgabriel081@gmail.com>
2023-02-02 18:44:41 -0300

    Classes ConsultaDAO, ProntuarioDAO e MedicoDAO

--- ClinicSystem--LPOO--Project/src/main/java/dao/MedicoDAO.java
+++ ClinicSystem--LPOO--Project/src/main/java/dao/MedicoDAO.java
@@ -1,7 +1,72 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import core.model.Medico;
+
 public class MedicoDAO {
 	
-	metodo retornar o id 
-
+FabricaConexao medicoDAO = new FabricaConexao();
+	
+    public List<Medico> listar() throws SQLException {
+    	String sql = "SELECT * FROM Medico";
+		List<Medico> retorno = new ArrayList();
+		PreparedStatement stmt = medicoDAO.getConexao().prepareStatement(sql);
+		ResultSet resultado = stmt.executeQuery();
+		while(resultado.next()) {
+			Medico medico = new Medico();
+			medico.setCpf(resultado.getString("CPF"));
+			medico.setNome(resultado.getString("Nome"));
+			medico.setTelefone(resultado.getString("Telefone"));
+			medico.setEmail(resultado.getString("Email"));
+			medico.setLogin(resultado.getString("Login"));
+			medico.setSenha(resultado.getString("Senha"));
+			medico.setCrm(resultado.getString("CRM"));
+			medico.setEspecialidade(resultado.getString("Especialidade"));
+			retorno.add(medico);
+		}
+		medicoDAO.fecharConexao();
+		return retorno;
+	}
+	
+	public void inserir(Medico medico) throws SQLException {
+		String sql = "INSERT INTO Medico(CPF, Nome, Telefone, Email, Login, Senha, CRM, Especialidade) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement stmt = medicoDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, medico.getCpf());
+		stmt.setString(2, medico.getNome());
+		stmt.setString(3, medico.getTelefone());
+		stmt.setString(4, medico.getEmail());
+		stmt.setString(5, medico.getLogin());
+		stmt.setString(6, medico.getSenha());
+		stmt.setString(7, medico.getCrm());
+		stmt.setString(8, medico.getEspecialidade());
+		stmt.execute();
+		medicoDAO.fecharConexao();
+	}
+	
+	public void alterar(Medico medico) throws SQLException {
+		String sql = "UPDATE Medico SET Nome=?, Telefone=?, Email=?, Login=?, Senha=?, CRM=?, Especialidade=? WHERE (CPF=?)";
+		PreparedStatement stmt = medicoDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, medico.getCpf());
+		stmt.setString(2, medico.getNome());
+		stmt.setString(3, medico.getTelefone());
+		stmt.setString(4, medico.getEmail());
+		stmt.setString(5, medico.getLogin());
+		stmt.setString(6, medico.getSenha());
+		stmt.setString(7, medico.getCrm());
+		stmt.setString(8, medico.getEspecialidade());
+		stmt.execute();
+		medicoDAO.fecharConexao();
+	}
+	
+	public void remover(Medico medico) throws SQLException {
+		String sql = "DELETE FROM Medico WHERE (CPF=?)";
+		PreparedStatement stmt = medicoDAO.getConexao().prepareStatement(sql);
+		stmt.setString(1, medico.getCpf());
+		stmt.execute();
+		medicoDAO.fecharConexao();
+	}
 }
