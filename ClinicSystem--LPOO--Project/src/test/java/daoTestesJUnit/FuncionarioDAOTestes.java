a0a5a12675ee1665b98518fb47511978a6be0dda
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2023-02-04 20:28:11 -0300

    adição do pacote de testes p/ camadas DAO/UI

---
+++ ClinicSystem--LPOO--Project/src/test/java/daoTestesJUnit/FuncionarioDAOTestes.java
@@ -0,0 +1,14 @@
+package daoTestesJUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FuncionarioDAOTestes {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ClinicSystem--LPOO--Project/src/test/java/uiTestesJUnit/Testes.java
@@ -0,0 +1,14 @@
+package uiTestesJUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testes {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
