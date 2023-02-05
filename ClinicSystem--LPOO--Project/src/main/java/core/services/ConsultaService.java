76e78f75ea0807c317e6dd14c4ec12c03961861c
sykes-07 <81064629+sykes-07@users.noreply.github.com>
2023-02-05 00:07:21 -0300

    metodosTotalConsultas

--- ClinicSystem--LPOO--Project/src/main/java/core/services/ConsultaService.java
+++ ClinicSystem--LPOO--Project/src/main/java/core/services/ConsultaService.java
@@ -47,16 +47,7 @@ public class ConsultaService {
 		ConsultaDAO consultadao = new ConsultaDAO();
 		
 		consulta.setTotalConsulta(consultadao.TotalConsultas());
-		
-		if(consulta.getTotalConsulta().equals(null)){
-			consulta.setTotalConsulta("0");			
-		}
 		return consulta.getTotalConsulta();
 		}
-	
-	public static void main(String[] args) throws SQLException {
-		ConsultaService consultaa = new ConsultaService(); 
-		System.out.print(consultaa.totalConsultasDia());
-	}
 	
 }
