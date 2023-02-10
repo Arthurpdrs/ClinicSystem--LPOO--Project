be5854a2219be1345cacdace81fdda2c220eab44
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2023-02-09 22:26:48 -0300

    incluído cenários de testes em ProntuarioService
    conclusão e adição de cenário de testes para o método .inserir()

--- ClinicSystem--LPOO-Project/testes/serviceTestesJUnit/ProntuarioServiceTestes.java
+++ ClinicSystem--LPOO-Project/testes/serviceTestesJUnit/ProntuarioServiceTestes.java
@@ -1,25 +1,64 @@
 package serviceTestesJUnit;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+
+import core.model.Prontuario;
 import core.services.ProntuarioService;
-import dao.ProntuarioDAO;
+import dao.FabricaConexao;
 
 class ProntuarioServiceTestes {
 
 	@Test
 	public void testeCadastrarOk () {
 		
-		ProntuarioService prontuario = new ProntuarioService();
-		ProntuarioDAO prontuarioDAO = new ProntuarioDAO();
+		ProntuarioService prontuarioTeste = new ProntuarioService();
+		
 				
 		try {
-			boolean cadastro = prontuario.inserir("dor", "OBSMedica", "01/01/2023", "prescricao", "11122233300", "Carlos", "Jose Maria", "Cardiologista");
+			boolean cadastro = prontuarioTeste.inserir("dor", "OBSMedica", "01/01/2023", "prescricao", "11122233300", "Carlos", "Jose Maria", "Cardiologista");
 			assertTrue(cadastro);
-//			prontuarioDAO.remover(prontuario);
+			
+			//excluíndo o registro inserido no banco de dados 
+			FabricaConexao prontuarioServiceTestes = new FabricaConexao();
+			Prontuario prontuario = new Prontuario();
+			String sql = "DELETE FROM Prontuario WHERE Paciente_CPF=?";
+			try {
+				PreparedStatement stmt = prontuarioServiceTestes.getConexao().prepareStatement(sql);
+				stmt.setString(1, prontuario.getPaciente().getCpf());
+				stmt.execute();
+				prontuarioServiceTestes.fecharConexao();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}					
 	}
+	
+	@Test
+	public void testeCadastrarValoresIsBlank () {
+		
+		ProntuarioService prontuarioTeste = new ProntuarioService();
+				
+		try {
+			boolean cadastro = prontuarioTeste.inserir("", "", "", "", "", "", "", "");
+			assertFalse(cadastro);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}					
+	}
+	
+	@Test
+	public void testeCadastrarValoresNull () {
+		
+		ProntuarioService prontuarioTeste = new ProntuarioService();
+		
+		assertThrows(NullPointerException.class,
+				() -> prontuarioTeste.inserir(null, null, null, null, null, null, null, null));
+	}
+	
 }
